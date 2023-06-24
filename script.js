580371a72249265a68853800753ddf257a043cb0
Hernando Abella <hernandoabella@gmail.com>
2023-06-24 02:55:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const tamano = document.getElementById("tamano");
 const texto = document.getElementById("texto");
-const body = document.body;
 
 const disminuir = () => {
   let tamanoTexto = Number(tamano.value);
   if (tamanoTexto > 20) {
-    tamano.value = tamanoTexto - 1;
+    tamano.value = tamanoTexto - 2;
     texto.style.fontSize = `${tamano.value}px`;
   }
   texto.style.animation = "none";
@@ -16,10 +15,18 @@ const disminuir = () => {
 const aumentar = () => {
   let tamanoTexto = Number(tamano.value);
   if (tamanoTexto < 40) {
-    tamano.value = tamanoTexto + 1;
+    tamano.value = tamanoTexto + 2;
     texto.style.fontSize = `${tamano.value}px`;
   }
   texto.style.animation = "none";
   void texto.offsetWidth;
   texto.style.animation = "aumentarVerde .3s";
 };
+
+const cambiarTamano = () => {
+  let tamanoTexto = Number(tamano.value);
+  texto.style.fontSize = `${tamanoTexto}px`;
+};
+
+document.getElementById("btnDisminuir").addEventListener("click", disminuir);
+document.getElementById("btnAumentar").addEventListener("click", aumentar);
